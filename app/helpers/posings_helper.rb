267875d1f4d78275293d8691686be7ca656cbc2b
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-15 20:28:41 +0900

    トップページ表示（本番環境で確認するため）

---
+++ app/helpers/posings_helper.rb
@@ -0,0 +1,2 @@
+module PosingsHelper
+end
---
+++ test/controllers/posings_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PosingsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
