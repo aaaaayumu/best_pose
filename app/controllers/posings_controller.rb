bbe0ddf5a5e1344d7bf5c0a1314f89a2c2b3052b
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-27 20:41:50 +0900

    コントローラーの設定

--- app/controllers/posings_controller.rb
+++ app/controllers/posings_controller.rb
@@ -7,5 +7,17 @@ class PosingsController < ApplicationController
   end
 
   def create
+    @posing = Posing.new(posing_params)
+    if @posing.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def posing_params
+    params.require(:posing).permit(:image, :group_id, :category_id, :type_id, :body_weight, :remarks).merge(user_id: current_user.id)
   end
 end
