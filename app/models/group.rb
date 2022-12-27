27a8e9870133411059471080ad339e5c91093ecc
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-27 12:30:46 +0900

    アクティブハッシュの導入

---
+++ app/models/group.rb
@@ -0,0 +1,16 @@
+class Group < ActiveHash::Base
+
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: 'JBBF' },
+    { id: 2, name: 'FWJ' },
+    { id: 3, name: 'ベストボディジャパン' },
+    { id: 4, name: 'サマースタイルアワード' },
+    { id: 5, name: 'NABBA' },
+    { id: 6, name: 'マッスルゲート' },
+    { id: 7, name: 'APF' },
+    { id: 8, name: 'NBBF' },
+    { id: 9, name: 'その他の団体' }
+  ]
+
+end
