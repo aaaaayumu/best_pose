f3b2d371f475ba68a8013b95dc547ce7496a89d6
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-27 13:08:56 +0900

    バリデーションの設定

--- app/models/posing.rb
+++ app/models/posing.rb
@@ -6,4 +6,7 @@ class Posing < ApplicationRecord
   belongs_to :group
   belongs_to :category
   belongs_to :type
+
+  validates :image, presence: true, blob: { content_type: :image }
+  validates :group_id, :category_id, :type_id, numericality: { other_than: 0, message: 'を選択してください' }
 end
