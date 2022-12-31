a3aa183bcd14436827eb822ed349dc96c55b01b4
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-31 13:12:19 +0900

    コントローラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = comment.new(comment_params)
+    @comment = Comment.new(comment_params)
+    @posing = Posing.find(params[:posing_id])
+    if @comment.save
+      redirect_to posing_path(@comment.posing.id)
+    else
+      render 'posings/show'
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: currnet_user.id, posing_id: params[:posing_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, posing_id: params[:posing_id])
   end
 end
