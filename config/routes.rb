6bb81c7c3eba8ab64543e80498fae9e52f91a934
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-31 12:17:51 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posings#index'
-  resources :posings, only: [:new, :create, :show]
+  resources :posings, only: [:new, :create, :show] do
+    resources :comments, only: :create
+  end
 end
