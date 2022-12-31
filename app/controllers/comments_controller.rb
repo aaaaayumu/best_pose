7efd86463f10d5adbd1c1cd83c294cb2cfdcb7d6
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2022-12-31 12:18:02 +0900

    コントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = comment.new(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: currnet_user.id, posing_id: params[:posing_id])
+  end
 end
