8ced49dcb91b574e5b5c49392abf5e74ebe90cd0
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2023-01-09 13:12:38 +0900

    set_posingメソッド定義

--- app/controllers/posings_controller.rb
+++ app/controllers/posings_controller.rb
@@ -1,5 +1,6 @@
 class PosingsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_posing, only: [:show, :edit, :update]
 
   def index
     @posings = Posing.includes(:user).order("created_at DESC")
@@ -19,17 +20,14 @@ class PosingsController < ApplicationController
   end
 
   def show
-    @posing = Posing.find(params[:id])
     @comment = Comment.new
     @comments = @posing.comments.includes(:user)
   end
 
   def edit
-    @posing = Posing.find(params[:id])
   end
 
   def update
-    @posing = Posing.find(params[:id])
     if @posing.update(posing_params)
       redirect_to posing_path(@posing)
     else
@@ -41,5 +39,9 @@ class PosingsController < ApplicationController
   
   def posing_params
     params.require(:posing).permit(:image, :group_id, :category_id, :type_id, :body_weight, :remarks).merge(user_id: current_user.id)
+  end
+
+  def set_posing
+    @posing = Posing.find(params[:id])
   end
 end
