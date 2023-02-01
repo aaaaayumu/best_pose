a551561918536371f88ed6471a80c6a7108cc0af
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2023-02-01 14:54:18 +0900

    ストロングパラメーターの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,20 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def show
     @user = User.find(params[:id])
     @posings = @user.posings
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confimation, :body_height, :body_weight, :self_introduction)
   end
 end
