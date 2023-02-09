988b4168237b2b6e065e99ecc62e9711981a1d71
aaaaayumu <112301397+aaaaayumu@users.noreply.github.com>
2023-02-09 12:15:01 +0900

    コントローラーの作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :comment_params, only: [:create, :destroy]
+
+  def create
+    Like.create(user_id: current_user.id, post_id: @comment.id)
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, post_id: @comment.id)
+    like.destroy
+  end
+
+  private
+  def comment_params
+    @comment = Comment.find(params[:comment_id])
+  end
+end
